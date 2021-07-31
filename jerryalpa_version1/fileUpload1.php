64ae83f404d9d29a0199a97fa528508cd60ab481
Jerry Li <84467756+jerryo0@users.noreply.github.com>
2021-07-31 11:34:19 -0400

    Update fileUpload1.php

--- jerryalpa_version1/fileUpload1.php
+++ jerryalpa_version1/fileUpload1.php
@@ -10,7 +10,7 @@ use Aws\S3\Exception\S3Exception;
 
 if(isset($_SESSION['userName']) && isset($_SESSION['time']) && isset($_SESSION['valid'])){
 // AWS Info
-$bucketName = 'test1-1';
+$bucketName = '';
 $IAM_KEY = '';
 $IAM_SECRET = '';
 
