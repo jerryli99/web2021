31128b58f4297c0a121c78a3cb849ba7e1bda45f
Jerry Li <84467756+jerryo0@users.noreply.github.com>
2021-09-16 09:05:29 -0400

    Update connectDB.php

--- jerryalpa_version1/connectDB.php
+++ jerryalpa_version1/connectDB.php
@@ -5,8 +5,8 @@ $dotenv= Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 // the dotenv worked!!!
-$connect = mysqli_connect('localhost', 'jerry1', '33678Jerry@ubuntu', 'demo2');
-//$connect = new mysqli('127.0.0.1:3307', 'root', '', 'exchangedemo');
+$connect = mysqli_connect('localhost', '', '', '');
+
 //check connection
 if($connect -> connect_error){
 //    die("Connection failed: " . $connect -> connect_error . "<br>");
